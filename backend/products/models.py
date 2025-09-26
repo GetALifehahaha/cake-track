9063497eb9ae5e1ca5ec4956b99ad74c7d0e5775
GetALifehahaha <sencioahlannour@gmail.com>
2025-09-26 12:13:56 +0800

    Create Products Models: Product, ProductStorage, Recipe, Ingredient, RecipeIngredient

---
+++ backend/products/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+
+# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    instructions = models.TextField()
+    prep_time = models.DurationField()
+    cook_time = models.DurationField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    # consisting of cakes, coffee, misc
+    # if misc, no recipe
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    has_recipe = models.BooleanField()
+    has_storage = models.BooleanField()
+
+    recipe = models.OneToOneField(
+        Recipe,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='product'
+    )
+
+
+class ProductStorage(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='storage'
+    )
+
+    quantity = models.IntegerField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    expiry_date = models.DateTimeField()
+    shelf_life = models.DurationField()
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    
+    UNIT = [
+        # weight
+        ('g', 'Grams'), ('kg', 'Kilograms'), ('sk', 'Sack'), ('bg', 'Bag'), ('bx', 'Box'), ('pk', 'Pack'),
+
+        # volume
+        ('ml', 'Milliliter'), ('l', 'Liter'), ('gl', 'Gallon'),
+
+        # count
+        ('pc', 'Piece'), ('dz', 'Dozen'), ('tr', 'Tray'),
+
+        # bottle
+        ('bt', 'Bottle'), ('cn', 'Can'),
+    ]
+
+    unit = models.CharField(choices=UNIT, max_length=15, default='g')
+
+
+class IngredientStorage(models.Model):
+    ingredients = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='ingredients'
+    )
+    
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    received_at = models.DateTimeField(auto_now_add=True)
+    expiry_date = models.DateTimeField()
+    shelf_life = models.DurationField()
+    
