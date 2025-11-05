6b561d0f1dfc17e273e9fb95a423b894b2352f31
GetALifehahaha <sencioahlannour@gmail.com>
2025-11-05 10:39:59 +0800

    Modified few variants, and add new for modals

--- frontend/src/components/atoms/Button.jsx
+++ frontend/src/components/atoms/Button.jsx
@@ -8,14 +8,17 @@ const Button = ({variant="block", size="base",  text="Button", icon: Icon, onCli
         outline: 'w-fit border border-gray-light text-gray rounded-xl font-semibold px-8 py-1 ',
         active: 'w-fit border border-accent-text bg-accent-text text-white rounded-full px-4 py-1 ',
         inactive: 'w-fit border border-gray-light bg-gray-light text-text rounded-full px-4 py-1 ',
-        base: "w-fit text-base font-semibold",
-        small: "w-fit text-sm "
+        base: "w-fit text-base",
+        small: "w-fit text-sm ",
+        modalSize: 'w-full',
+        modalOutline: "font-medium border-border border rounded-lg p-2 text-text",
+        modalBlock: "font-medium border-border border rounded-lg p-2 text-main-white bg-gray-dark"
     }
     
     return (
         <button 
         onClick={onClick}
-        className={`flex gap-4 items-center cursor-pointer ${buttonVariants[size]} ${buttonVariants[variant]}`}>
+        className={`flex gap-4 items-center justify-center cursor-pointer ${buttonVariants[size]} ${buttonVariants[variant]}`}>
             {Icon && <Icon width={18}/>}
             {text}
         </button>
