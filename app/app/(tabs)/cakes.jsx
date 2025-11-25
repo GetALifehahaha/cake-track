b93f4ec48f9addba05c10d1258c3ff601d4b5325
GetALifeHahaha <sencioahlannour@gmail.com>
2025-11-25 23:00:42 +0800

    setup cakes ui

--- app/app/(tabs)/cakes.jsx
+++ app/app/(tabs)/cakes.jsx
@@ -1,10 +1,36 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
+import {Cake, Sparkles} from 'lucide-react-native'
 import React from 'react'
 
 const Cakes = () => {
   return (
-    <View>
-      <Text>cakes</Text>
+    <View className='flex-1 bg-white'>
+      {/* Header */}
+      <View className='pt-24 pb-8 w-full border-b border-b-gray-300 bg-white'>
+        <Text className='font-bold text-3xl text-center'>Cakes</Text>
+      </View>
+
+      <View className='mt-12 w-full items-center gap-2'>
+        <Text className='text-[#654321] text-xl font-bold'>Choose Cake Type</Text>
+        <Text className='text-[#9A8978] font-medium'>What would you like to order?</Text>
+      </View>
+
+      <View className='flex-1 items-center justify-evenly pt-12 pb-24 gap-6'>
+        <TouchableOpacity className='border-4 border-[#D4C7B8] aspect-square w-48 rounded-xl items-center justify-center gap-2'>
+          <View className='bg-[#A67C52] rounded-full w-16 h-16 items-center justify-center'>
+            <Cake style={{color: "white"}} size={28} />
+          </View>
+          <Text className='text-[#654321] font-semibold text-xl'>Pre-made</Text>
+          <Text className='text-[#9A8978]'>Ready-made designs</Text>
+        </TouchableOpacity>
+        <TouchableOpacity className='border-4 border-[#D4C7B8] aspect-square w-48 rounded-xl items-center justify-center gap-2'>
+          <View className='bg-[#A67C52] rounded-full w-16 h-16 items-center justify-center'>
+            <Sparkles style={{color: "white"}} size={28} />
+          </View>
+          <Text className='text-[#654321] font-semibold text-xl'>Custom</Text>
+          <Text className='text-[#9A8978] text-center'>Make your own or let the Baker surprise you</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- app/app/(tabs)/index.jsx
+++ app/app/(tabs)/index.jsx
@@ -2,11 +2,26 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 const Index = () => {
-  return (
-    <View>
-      <Text className='text-blue-500'>Home</Text>
-    </View>
-  )
+	return (
+		<View className='bg-[#8B5A3C] flex-1'>
+			<View className='bg-white w-full mt-auto rounded-t-[2rem] h-[80vh]'>
+				{/* Header */}
+				<View className='p-6 w-full'>
+					<Text className='text-[#474747] text-3xl font-bold'>Michelle's Cake & Cafe</Text>
+					<View className='flex-row'>
+						<Text className='font-bold text-xl text-[#6B5235]'>Cake</Text>
+						<Text className='font-bold text-xl text-[#BE9B7B]'>Track</Text>
+					</View>
+				</View>
+
+
+				{/* Carousel */}
+				<View>
+					<Text></Text>
+				</View>
+			</View>
+		</View>
+)
 }
 
 export default Index
