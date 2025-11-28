a75b5cd421b21e2f918acd558d9960e5039a02a4
Justine Toong <programmerjustine@gmail.com>
2025-11-28 21:06:27 +0800

    fetch order, render, and adjusted confirmation modal

--- frontend/src/pages/Queue/QueueOverview.jsx
+++ frontend/src/pages/Queue/QueueOverview.jsx
@@ -2,26 +2,18 @@ import React, { useState } from 'react'
 import { Button, Title } from '../../components/atoms'
 import { ArrowRight, Check, X } from 'lucide-react'
 import { useNavigate } from 'react-router-dom'
-import { ConfirmationModal } from '@/components/organisms'
+import { ConfirmationModal, ConfirmationModalWrapper } from '@/components/organisms'
 import useOrder from '@/hooks/useOrders'
 import Loading from '@/components/molecules/Loading'
 
 const QueueOverview = () => {
 
-	const { fetchOrders, loading, error } = useOrder();
+	const { data, loading, error, patchOrder } = useOrder();
 	const navigate = useNavigate();
-	const [pendingData, setPendingData] = useState([])
-	const [acceptConfirmation, setAcceptConfirmation] = useState(false);
-	const [rejectConfirmation, setRejectConfirmation] = useState(false);
-	const [removeId, setRemoveId] = useState();
+
+	const [removeId, setRemoveId] = useState(-1);
 
 	if (loading) return <Loading />
-
-	const showAccept = (id) => { setAcceptConfirmation(true); setRemoveId(id) }
-	const closeAccept = (id) => { setAcceptConfirmation(false); setRemoveId() }
-	const showReject = (id) => { setRejectConfirmation(true); setRemoveId(id) }
-	const closeReject = (id) => { setRejectConfirmation(false); setRemoveId() }
-	const removeOrder = () => { setPendingData(pendingData.filter(data => data.id != removeId)); closeAccept(); closeReject(); }
 
 	const acceptedOrdersHeaders = [
 		"ID", "Name", "Cake Flavor", "w/ Cupcake", "Placement Order", "Due Date"
@@ -34,15 +26,28 @@ const QueueOverview = () => {
 		expired: 0
 	}
 
-	// const listPending = pendingData.map((data, index) =>
-	// 	<div key={index} className='flex justify-between w-full'>
-	// 		<h5>{data.client}</h5>
-	// 		<h5>{data.cake.name}</h5>
-	// 		<h5>{data.date}</h5>
-	// 		<button className='text-error cursor-pointer' onClick={() => showReject(data.id)}><X size={18} /></button>
-	// 		<button className='text-success cursor-pointer' onClick={() => showAccept(data.id)}><Check size={18} /></button>
-	// 	</div>
-	// )
+	const listPending = data.results.filter((item) => item.status.toLowerCase() == "pending").slice(0, 5).map((order, index) =>
+		<div key={index} className='flex justify-between w-full'>
+			<h5>{order.full_name}</h5>
+			<h5>{order.cake_orders.occassion}</h5>
+			<h5>{order.due_date}</h5>
+			<div className='flex items-center gap-4'>
+				<ConfirmationModalWrapper title='Reject order' content='Please confirm that you wish to reject this order. You will be required to provide a reason for the rejection.' onReject={() => setRemoveId(-1)}>
+					<X className='text-red-500' onClick={() => setRemoveId(item.id)} size={18} />
+				</ConfirmationModalWrapper>
+				<ConfirmationModalWrapper title='Accept order' content={`Confirm acceptance of Order #${order.id}. This action is irreversible and places the order into the active production schedule.`} onReject={() => setRemoveId(-1)}>
+					<Check className='text-green-500' size={18} />
+				</ConfirmationModalWrapper>
+			</div>
+		</div>
+	)
+	const listAccepted = data.results.filter((item) => item.status.toLowerCase() == "accepted").slice(0, 5).map((order, index) =>
+		<div key={index} className='flex justify-between w-full'>
+			<h5>{order.full_name}</h5>
+			<h5>{order.cake_orders.occassion}</h5>
+			<h5>{order.due_date}</h5>
+		</div>
+	)
 
 	const listAcceptedOrdersHeaders = acceptedOrdersHeaders.map((header, index) => <h5 key={index} className='text-accent-mute text-sm basis-1/6 text-center'>{header}</h5>)
 
@@ -54,7 +59,7 @@ const QueueOverview = () => {
 					<div className='flex justify-between items-center pb-2 border-b border-b-border'>
 						<div className='flex gap-2'>
 							<Title variant='modal' text='Pending' />
-							<h5 className='font-semibold text-text/50'>{pendingData.length}</h5>
+							<h5 className='font-semibold text-text/50'>{ }</h5>
 						</div>
 						<button className='flex items-center gap-2 text-accent cursor-pointer' onClick={() => navigate('/queue/pending')}><h5>View All</h5><ArrowRight size={16} /></button>
 					</div>
@@ -62,7 +67,7 @@ const QueueOverview = () => {
 					{/* Pending Body */}
 					<div>
 						<div className='flex flex-col gap-2 p-2 text-xs font-medium'>
-							{/* {listPending} */}
+							{listPending}
 						</div>
 					</div>
 				</div>
@@ -76,7 +81,7 @@ const QueueOverview = () => {
 
 					{/* Due Soon Body */}
 					<div>
-
+						{listAccepted}
 					</div>
 				</div>
 
@@ -118,13 +123,6 @@ const QueueOverview = () => {
 					{listAcceptedOrdersHeaders}
 				</div>
 			</div>
-
-			{acceptConfirmation &&
-				<ConfirmationModal title="Accept Order?" content="This will be added to your accepted orders" onConfirm={removeOrder} onReject={closeAccept} />
-			}
-			{rejectConfirmation &&
-				<ConfirmationModal title="Reject Order?" content="" onConfirm={removeOrder} onReject={closeReject} />
-			}
 		</div>
 	)
 }
