806ee8fbae4ba1431ef56a5dd6a8aa9d572f4410
GetALifeHahaha <sencioahlannour@gmail.com>
2026-01-24 06:38:36 +0800

    revamp select variant UI

--- frontend/src/components/molecules/ModalBody.jsx
+++ frontend/src/components/molecules/ModalBody.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Title } from '../atoms'
+import { X } from 'lucide-react'
 import { cn } from '@/utils/cn'
 
-const ModalBody = ({ children, className }) => {
+const ModalBody = ({ children, className, title="Modal", onClose, subtitle='' }) => {
   return (
     <div className='absolute top-0 left-0 w-full bg-black/10 backdrop-blur-sm h-screen flex justify-center items-center z-10'>
       <div className={cn('p-6 bg-main-white rounded-xl shadow-md shadow-black/25 min-w-[30vw] max-w-[90vw] max-h-[80vh] flex flex-col gap-10', className)}>
+        <div className='flex justify-between items-start w-full'>
+          <div className='flex flex-col gap-1 items-start'>
+              <Title variant='modal' text={title} />
+              {subtitle.length > 0 &&
+                <h5 className='text-text/50 text-sm font-medium'>{subtitle}</h5>
+              }
+          </div>
+
+          <X size={16} className='text-text cursor-pointer' onClick={onClose}/>
+          </div>
         {children}
       </div>
     </div>
--- frontend/src/components/organisms/pos/VariantModal.jsx
+++ frontend/src/components/organisms/pos/VariantModal.jsx
@@ -1,31 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ModalBody } from '../../molecules';
-import { Title } from '../../atoms';
-import { X } from 'lucide-react';
+import { Check } from 'lucide-react';
+import { cn } from '@/utils/cn';
+import { Button } from '@/components/atoms';
 
 const VariantModal = ({product, onClose, onChoose}) => {
-    return (
-        <ModalBody>
-            <div className='flex justify-between items-center w-full'>
-                <Title variant='modal' text='Variants' />
-                <X size={16} className='text-text cursor-pointer' onClick={onClose}/>
-            </div>
 
-            <div className='flex gap-2'>
+    const [selected, setSelected] = useState({id: null, variant: null});
+
+    const selectVariant = () => {
+        onChoose(selected.variant)
+    }
+
+    const selectOption = (id, variant) => {
+        selected === id ? setSelected({id: null, variant: null}) : setSelected({id: id, variant: variant})
+    }
+
+    return (
+        <ModalBody title='Select Size' subtitle={product.name} onClose={onClose}>
+            <div className='flex flex-col gap-2'>
                 {product?.variants?.map(({id, label, price}) => 
                     <div 
                     key={id} 
-                    className='flex flex-col gap-2 items-center p-2.5 rounded-md border border-border basis-1/5 cursor-pointer hover:bg-border'
-                    onClick={() => {onChoose({...product, variant_id: id, label: label, price: price, amount: 1}); onClose()}}
+                    className={cn('flex flex-row gap-2 items-center p-3.5 rounded-md border-2 border-border/75 basis-1/5 cursor-pointer hover:bg-border', selected.id==id && 'border-accent-mute bg-main-dark/25')}
+                    onClick={() => {selectOption(id, {...product, variant_id: id, label: label, price: price, amount: 1})}}
                     >
-                        <h5 className='font-bold text-md text-text'>{label}</h5>
+                        <div className={cn('flex items-center justify-center rounded-full border-gray border p-0.5', selected.id==id && 'bg-accent-mute border-accent-mute')}>
+                            <Check size={14} className={cn('text-white opacity-0', selected.id == id && 'opacity-100')} />
+                        </div>
+                        <h5 className='font-semibold text-md text-text'>{label}</h5>
 
-                        <h5 className='font-semibold text-text/75'>₱ {price}</h5>
+                        <h5 className='ml-auto font-semibold text-accent-mute'>₱ {price}</h5>
                     </div>
                 )}
                 {product.variants.length === 0 &&
-                    <h5 className='font-medium text-text/50 mx-auto text-sm'>No Product Variants to Show</h5>
+                    <h5 className='font-medium text-text/50 mx-auto text-sm'>There are no product variants yet</h5>
                 }
+                <div className='flex gap-2 mt-8 ml-auto'>
+                    <Button text='Cancel' variant='modalOutline' size='small' onClick={selectVariant} />
+                    <Button text='Add Order' variant='modalBlock' className='bg-accent-dark' size='small' onClick={selectVariant} />
+                </div>
             </div>
         </ModalBody>
     )
