8a261892de3b52b29418c68ac990d80c006ec0d8
GetALifeHahaha <sencioahlannour@gmail.com>
2026-02-02 18:48:16 +0800

    refactor: Dashboard Api

--- frontend/src/api/DashboardApi.js
+++ frontend/src/api/DashboardApi.js
@@ -1,13 +1,17 @@
 import api from "./api";
 
-const DashboardApi = async (params = {}) => {
-    try {
-        // Matches the 'path('dashboard/analytics/', ...)' from your Django views
-        const response = await api.get('/pos/analytics/', { params });
-        return response.data;
-    } catch (err) {
-        throw err;
-    }
-};
+// const DashboardApi = async (params = {}) => {
+//     try {
+//         // Matches the 'path('dashboard/analytics/', ...)' from your Django views
+//         const response = await api.get('/pos/analytics/', { params });
+//         return response.data;
+//     } catch (err) {
+//         throw err;
+//     }
+// };
 
-export default DashboardApi;
+// export default DashboardApi;
+
+export const DashboardApi = {
+    fetchList: (params) => api.get('/pos/analytics/', {params}).then(res => res.data)
+}
