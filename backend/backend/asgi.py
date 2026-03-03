5b7aab58dfc6f394fedfab7319159353491ce7be
Ahlan-nour <sencioahlannour@gmail.com>
2026-03-04 02:23:23 +0800

    migrate fix

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+import django
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_asgi_application()
+
+
+django.setup()
+call_command("migrate", interactive=False)
